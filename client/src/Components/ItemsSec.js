cb089b7f7a06f2458f019d26e3ad400695dc58aa
JoeyParlamenti <Joeyparlamenti@yahoo.com>
2022-10-15 14:20:07 -0400

    Set up initial ItemBoard skeleton

--- client/src/Components/ItemsSec.js
+++ client/src/Components/ItemsSec.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ItemBoards from './ItemBoards'
 
 function ItemsSec() {
 
@@ -9,10 +10,40 @@ function ItemsSec() {
         <h3> Items </h3>
         <p> Insert description about items and use here</p>
 
+          {/* Chapter selection filter */}
+        <select>
+          <option>
+            All
+          </option>
+          <option>
+            Chapter 1
+          </option>
+          <option>
+            Chapter 2
+          </option>
+          <option>
+            Chapter 3
+          </option>
+          <option>
+            Chapter 4
+          </option>
+          <option>
+            Chapter 5
+          </option>
+          <option>
+            Chapter 6
+          </option>
+
+        </select>
+        
         {/* insert item list here with filter search bar */}
 
-
-
+        <div>
+          {/* itemArray.filter(filtItemArr => {
+            return {< ItemBoards filtItemArr={filtItemArr} }
+          }) */}
+          <ItemBoards />
+        </div>
 
 
     </div>
