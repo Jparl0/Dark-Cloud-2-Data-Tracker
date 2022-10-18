d801076f047703073277d7c0e992ae9a44ce6842
JoeyParlamenti <Joeyparlamenti@yahoo.com>
2022-10-17 22:53:02 -0400

    updated home page

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -10,7 +10,16 @@ function HomePage() {
     
   return (
     <div>
+        <header>
+          <h2>
+            Dark Cloud 2 Data Manager
+          </h2>
+          <p>
+            This site is designed to allow users playing this game to visualize and manage the extensive data provide. 
+            Choosing a weapon path, managing an ever-increasinly complex inventory, and planning your route for success is available through this application.
+          </p>
 
+        </header>
 
       <div>
         
